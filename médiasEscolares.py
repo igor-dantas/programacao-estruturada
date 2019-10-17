485901993ff763186791ad2dd8c2fb312782243b
igor-dantas <55963347+igor-dantas@users.noreply.github.com>
2019-10-17 16:44:07 -0300

    Create médiasEscolares.py

---
+++ médiasEscolares.py
@@ -0,0 +1,7 @@
+
+n1 = float(input("Digite a primeira nota:"))
+n2 = float(input("Digite uma segunda nota:"))
+n3 = float(input("Digite uma terceira nota:"))
+n4 = float(input("Digite uma quarta nota:"))
+print("A média aritmética é", (n1 + n2 + n3 + n4)/4)
+print("a soma das notas é", n1 + n2+ n3 + n4)
